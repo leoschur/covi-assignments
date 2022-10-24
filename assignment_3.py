3f62f23ec55641fc2d827360427758e7865135dd
Leo Schurrer <l.schurrer@oth-aw.de>
2022-10-24 10:58:17 +0200

    add: assignment_3

---
+++ assignment_3.py
@@ -0,0 +1,28 @@
+# %% imports
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+# %% task 1
+#  Schreiben Sie ein Programm, wo das Bild (circles1.png) eingelesen und mit matplotlib gezeigt wird.
+
+# %% task 2
+# Segmentieren Sie alle Kreise und speichern Sie die Maske (das Binärbild) ab.
+
+# %% task 3
+# Segmentieren Sie jetzt Kreise, die die gleichen Intensitätswerte haben. Wie viele Masken bekommen Sie in diesem Fall? Speichern Ihre Ergebnisse ab.
+
+# %% task 4
+# Finden Sie die Raumkoordinate vom Pixel P, das im Zentrum des Bildes liegt. Segmentieren Sie nur die Kreise, die rechts unten von dem P liegen. Visualisieren Sie Ihre Ergebnisse.
+
+# %% task 5
+# Berechnen Sie die Connected Components auf den Masken und plotten Sie nur die größte Region in jeder Maske.
+
+# %% task 6
+# Schreiben Sie ein Programm, wo das Bild (circles_rauschen.png) eingelesen und mit matplotlib gezeigt wird.
+
+# %% task 7
+# Versuchen Sie jetzt die Kreise zu segmentieren (Aufgaben 2-5).
+
+# %% task 8
+# Wie gut sind Ihre Segmentierungsergebnisse? Quantifizieren Sie Ihre Aussage. Hinweis: Was können Sie als Gold Standard nutzen?
